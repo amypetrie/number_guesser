91d0e9a7b62df85c31e80be224b4215d979e1cf3
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-01-17 11:40:49 -0700

    initial commit plus basic html for page elements

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// add constants
+// add functions
+// add event listeners
+
+const guess = document.querySelector('.guess');
+
+document.getElementById("submitGuess").addEventListener("click", function(){
+  document.getElementById("mostRecentGuess").innerHTML = "You guessed xyz";
+});
+
+  // add event listener for guess click
+
+  // select the users guess by the div tag
+
+  //
