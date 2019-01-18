adc431bc42cda6c969844b969a652c8316b401b0
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-01-17 22:44:11 -0700

    finishes phase 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 var submitGuessButton = document.getElementById("submitGuess");
+var submitRangeButton = document.getElementById("submitRange");
 var clearGuessButton = document.getElementById("clearGuess");
 var resetGameButton = document.getElementById("resetGame");
 var guessInputField = document.getElementById("userGuessInput");
@@ -11,13 +12,17 @@ var userGuessCounter = 0;
 window.onload = setFormNumberRange();
 
 function resetCurrentGame(){
-  resetWebPageContent();
   resetGameVariables();
+  resetWebPageContent();
 }
 
 function resetWebPageContent(){
-  clearGuessInput();
   resetGameButton.disabled = true;
+  clearGuessContent();
+}
+
+function clearGuessContent() {
+  clearGuessInput();
   document.getElementById("mostRecentGuess").innerHTML = "";
   document.getElementById("guessFeedback").innerHTML = "";
 }
@@ -25,6 +30,7 @@ function resetWebPageContent(){
 function resetGameVariables(){
   min_num = 0;
   max_num = 100;
+  setFormNumberRange();
   correctNumber = generateRandomNumber(min_num, max_num);
   userGuessCounter = 0;
 }
@@ -37,6 +43,7 @@ function generateRandomNumber(min, max){
 function setFormNumberRange(){
   guessInputField.min = min_num;
   guessInputField.max = max_num;
+  document.getElementById("rangeInformation").innerHTML = `Your range is <b>${min_num}</b> to <b>${max_num}</b>.`;
 }
 
 function disableGuessButtons(){
@@ -54,9 +61,25 @@ function clearGuessInput(){
   disableGuessButtons();
 }
 
+function clearRangeInput(){
+  document.getElementById("userCreateRangeForm").reset();
+  submitRangeButton.disabled = true;
+}
+
 function changeInputToNumber(input_value){
   return parseInt(input_value, 10);
 } 
+
+function verifyRangeInput(){
+  let range_min_value = changeInputToNumber(userMinNumInput.value);
+  let range_max_value = changeInputToNumber(userMaxNumInput.value);
+  if(range_min_value < range_max_value){
+    submitRangeButton.disabled = false;
+  }
+  else{
+    submitRangeButton.disabled = true;
+  }
+}
 
 function verifyUserInput(){
   let user_input = changeInputToNumber(userGuessInput.value);
@@ -68,7 +91,16 @@ function verifyUserInput(){
   }
 }
 
+function processRangeChange(){
+  clearGuessContent();
+  min_num = changeInputToNumber(userMinNumInput.value);
+  max_num = changeInputToNumber(userMaxNumInput.value);
+  setFormNumberRange();
+  correctNumber = generateRandomNumber(min_num, max_num);
+}
+
 function processGuess(){
+  clearRangeInput();
   userGuessCounter++;
   currentUserGuess = guessInputField.value;
   displayUserGuessContent();
@@ -81,29 +113,32 @@ function displayUserGuessContent(){
 }
 
 function displayGuess(){
-  document.getElementById("mostRecentGuess").innerHTML = `You guessed ${currentUserGuess}`;
+  document.getElementById("mostRecentGuess").innerHTML = `You guessed <b>${currentUserGuess}</b>`;
 }
 
 function displayGuessFeedback() {
+  document.getElementById("rangeInformation").innerHTML = "";
   if(currentUserGuess == correctNumber){
-    document.getElementById("guessFeedback").innerHTML = "BOOM!";
-    createNewRange();
+    document.getElementById("guessFeedback").innerHTML = "BOOM! <b>That's right!</b> Let's play again...";
+    createHarderRange();
   }
   else if(currentUserGuess < correctNumber){
-    document.getElementById("guessFeedback").innerHTML = "Your guess is too low!";
+    document.getElementById("guessFeedback").innerHTML = "Your guess is <b>too low!</b>";
   }
   else if(currentUserGuess > correctNumber){
-    document.getElementById("guessFeedback").innerHTML = "Your guess is too high!";
+    document.getElementById("guessFeedback").innerHTML = "Your guess is <b>too high!</b>";
   }
 }
 
-function createNewRange(){
+function createHarderRange(){
   min_num = (min_num - 10);
   max_num = (max_num + 10);
   setFormNumberRange();
+  correctNumber = generateRandomNumber(min_num, max_num);
 }
 
 submitGuessButton.addEventListener('click',processGuess);
 clearGuessButton.addEventListener('click',clearGuessInput);
 resetGameButton.addEventListener('click',resetCurrentGame);
+submitRangeButton.addEventListener('click',processRangeChange);
 
