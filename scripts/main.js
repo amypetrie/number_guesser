b5f097b26dafc14ce4e34e047a22368dc0e59e60
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-01-17 19:11:37 -0700

    finishes phase 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,93 @@
 var submitGuessButton = document.getElementById("submitGuess");
+var clearGuessButton = document.getElementById("clearGuess");
+var resetGameButton = document.getElementById("resetGame");
+var guessInputField = document.getElementById("userGuessInput");
 var min_num = 0;
 var max_num = 100;
 var correctNumber = generateRandomNumber(min_num, max_num);
 var currentUserGuess;
+var userGuessCounter = 0;
+
+window.onload = setFormNumberRange();
+
+function resetCurrentGame(){
+  resetWebPageContent();
+  resetGameVariables();
+}
+
+function resetWebPageContent(){
+  clearGuessInput();
+  resetGameButton.disabled = true;
+  document.getElementById("mostRecentGuess").innerHTML = "";
+  document.getElementById("guessFeedback").innerHTML = "";
+}
+
+function resetGameVariables(){
+  min_num = 0;
+  max_num = 100;
+  correctNumber = generateRandomNumber(min_num, max_num);
+  userGuessCounter = 0;
+}
+
+function generateRandomNumber(min, max){
+  let result = Math.random() * (max-min) + min;
+  return Math.floor(result);
+}
+
+function setFormNumberRange(){
+  guessInputField.min = min_num;
+  guessInputField.max = max_num;
+}
+
+function disableGuessButtons(){
+  submitGuessButton.disabled = true;
+  clearGuessButton.disabled = true;
+}
+
+function enableGuessButtons(){
+  submitGuessButton.disabled = false;
+  clearGuessButton.disabled = false;
+}
+
+function clearGuessInput(){
+  document.getElementById("userGuessForm").reset();
+  disableGuessButtons();
+}
+
+function changeInputToNumber(input_value){
+  return parseInt(input_value, 10);
+} 
+
+function verifyUserInput(){
+  let user_input = changeInputToNumber(userGuessInput.value);
+  if(user_input >= min_num && user_input <= max_num){
+    enableGuessButtons();
+  }
+  else{
+    disableGuessButtons();
+  }
+}
+
+function processGuess(){
+  userGuessCounter++;
+  currentUserGuess = guessInputField.value;
+  displayUserGuessContent();
+}
+
+function displayUserGuessContent(){
+  resetGameButton.disabled = false;
+  displayGuess();
+  displayGuessFeedback();
+}
 
 function displayGuess(){
-  currentUserGuess = document.getElementById("userGuessInput").value;
   document.getElementById("mostRecentGuess").innerHTML = `You guessed ${currentUserGuess}`;
-  displayGuessFeedback();
 }
 
 function displayGuessFeedback() {
   if(currentUserGuess == correctNumber){
     document.getElementById("guessFeedback").innerHTML = "BOOM!";
-  } 
-  else if(currentUserGuess < min_num || currentUserGuess > max_num){
-    document.getElementById("guessFeedback").innerHTML = "Your guess is out of range!";
+    createNewRange();
   }
   else if(currentUserGuess < correctNumber){
     document.getElementById("guessFeedback").innerHTML = "Your guess is too low!";
@@ -25,9 +97,13 @@ function displayGuessFeedback() {
   }
 }
 
-function generateRandomNumber(min, max){
-  let result = Math.random() * (max-min) + min;
-  return Math.floor(result);
+function createNewRange(){
+  min_num = (min_num - 10);
+  max_num = (max_num + 10);
+  setFormNumberRange();
 }
 
-submitGuessButton.addEventListener('click',displayGuess);
+submitGuessButton.addEventListener('click',processGuess);
+clearGuessButton.addEventListener('click',clearGuessInput);
+resetGameButton.addEventListener('click',resetCurrentGame);
+
